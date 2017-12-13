f0d08b0679fc5b2617cb8ee22d57a26d14eaefd5
Irsyad <irsyart@gmail.com>
2017-12-13 17:52:20 +0700

    Added: hamming-distance solution

---
+++ hamming-distance.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/hamming-distance/description/
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x ^ y).count('1')
