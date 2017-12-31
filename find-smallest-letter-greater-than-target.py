c52161185a684649f54fd783a0f01872e0bc1fca
irxd <irsyart@gmail.com>
2017-12-31 19:24:34 +0700

    Added: find-smallest-letter-greater-than-target solution

---
+++ find-smallest-letter-greater-than-target.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for letter in letters:
+            if letter > target:
+                return letter
+        return letters[0]
